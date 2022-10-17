9a004d92ca104c093c8bb56635b1bb5053aa57a2
Precious Chika <43546663+pre22@users.noreply.github.com>
2022-10-17 02:33:32 +0100

    chore: views, templates
    created views, home and update payment template

--- payments/views.py
+++ payments/views.py
@@ -1,4 +1,8 @@
-from django.views.generic import TemplateView 
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from payments.models import Salary
 
-class PaymentView(TemplateView):
+class PaymentView(CreateView):
+    models = CreateView
     template_name = "payments/payment.html"
+    success_url = reverse_lazy("home")

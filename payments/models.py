58f419fe18e72298014e5e3976b5c915d11534f2
Precious Chika <43546663+pre22@users.noreply.github.com>
2022-10-17 02:20:15 +0100

    created models

--- payments/models.py
+++ payments/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from djmoney.models.fields import MoneyField
 
 # Create your models here.
+
+ROLE = (
+    ("SOFTWARE ENGINEER"), ("SOFTWARE ENGINEER"),
+    ("MECHANICAL ENGINEER"), ("MECHANICAL ENGINEER")
+)
+
+class Salary(models.Model):
+    fullname = models.CharField(max_length=50)
+    role = models.CharField(max_length=50, choices=ROLE)
+    amount = MoneyField(max_digits=14, decimal_places=2, default_currency='USD')
+
+    def __str__(self):
+        return self.fullname
+        
+    

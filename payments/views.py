890c44d512eb33d44523fe052fd3fe0ad5df2968
Precious Chika <43546663+pre22@users.noreply.github.com>
2022-10-17 01:16:56 +0100

    creaed route and views

--- payments/views.py
+++ payments/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView 
 
-# Create your views here.
+class PaymentView(TemplateView):
+    template_name = "payments/payment.html"

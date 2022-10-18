251810223dbfcfacb86838d9a50c05939b8c661e
Precious Chika <43546663+pre22@users.noreply.github.com>
2022-10-18 04:54:13 +0100

    fixed model issue in createVIew
    refxied templates

--- payments/views.py
+++ payments/views.py
@@ -3,6 +3,12 @@ from django.urls import reverse_lazy
 from payments.models import Salary
 
 class PaymentView(CreateView):
-    models = CreateView
+    model = Salary
+    fields = "__all__"
     template_name = "payments/payment.html"
     success_url = reverse_lazy("home")
+
+    # def form_valid(self, form):
+    #     form.instance.employee = self.request.user.employee
+    #     form.instance.company = self.request.user.employee.company
+    #     return super().form_valid(form)  
